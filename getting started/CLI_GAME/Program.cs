6cd65050ce977c35be35b2028323b4ea7b0d0d3b
LEONARDO DA SILVA DE JESUS <techopcgamer@gmail.com>
2024-06-04 12:50:15 -0300

    Initial commit

---
+++ getting started/CLI_GAME/Program.cs
@@ -0,0 +1,37 @@
+﻿//cli rock paper scissors game
+
+using Microsoft.Win32.SafeHandles;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jogo de Pedra, Papel, Tesoura");
+            Console.WriteLine("Digite 1 para Pedra, 2 para Papel, ou 3 para Tesoura");
+            int playerChoice = int.Parse(Console.ReadLine());
+
+            string[] choices = { "Pedra", "Papel", "Tesoura" };
+
+            int computerChoice = new Random().Next(1, 3);
+
+            Console.WriteLine($"Voce escolheu {choices[playerChoice - 1]}");
+            Console.WriteLine($"O computador escolheu {choices[computerChoice - 1]}");
+
+            if (playerChoice == computerChoice)
+            {
+                Console.WriteLine("Empate");
+            }
+            else if (playerChoice == 1 && computerChoice == 3 || playerChoice == 2 && computerChoice == 1 || playerChoice == 3 && computerChoice == 2)
+            {
+                Console.WriteLine("Jogador vence");
+            }
+            else
+            {
+                Console.WriteLine("Computador vence");
+            }
+            
+        }
+    }
+}
---
+++ getting started/getting_started/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SQLite;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name;
+        int age;
+        Console.WriteLine("What is your name?");
+        name = Console.ReadLine();
+        Console.WriteLine("How old are you?");
+        age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Hi " + name + ", You are " + age + " years old");
+
+
+        //save name and age to database using sqlite data.db
+        //check if the table exists, if not create it
+        using (var connection = new SQLiteConnection("Data Source=data.db"))    
+        {
+            connection.Open();
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE IF NOT EXISTS People (name TEXT, age INTEGER)";
+                command.ExecuteNonQuery();
+                command.CommandText = "INSERT INTO People VALUES(@name, @age)";
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@age", age);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
