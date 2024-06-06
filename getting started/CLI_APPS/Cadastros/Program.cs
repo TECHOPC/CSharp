26d9d08a9dbe149740643086056692c64c124646
LEONARDO DA SILVA DE JESUS <techopcgamer@gmail.com>
2024-06-06 11:46:53 -0300

    CLI-Cadastros
    funções para remover e editar adicionadas
    
    remove and edit functions added

--- getting started/CLI_APPS/Cadastros/Program.cs
+++ getting started/CLI_APPS/Cadastros/Program.cs
@@ -3,7 +3,7 @@
 class Program
 {
     static Dictionary<int, string[]> person_data = new Dictionary<int, string[]>();
-    static string[] temp = new string[] {};
+    static string[] temp = [];
 
     static void Main(string[] args)
     {   
@@ -70,14 +70,12 @@ class Program
                 Add_register();
                 break;
             case "2":
-                Console.WriteLine("2 - Remover Cadastro - WIP");
-                Thread.Sleep(1000);
-                Manage_registers();
+                Console.WriteLine("2 - Remover Cadastro");
+                Remove_register();
                 break;
             case "3":
-                Console.WriteLine("3 - Alterar Cadastro - WIP");
-                Thread.Sleep(1000);
-                Manage_registers();
+                Console.WriteLine("3 - Alterar Cadastro");
+                Edit_register();
                 break;
             case "4":
                 Console.WriteLine("4 - Visualizar Cadastro");
@@ -146,7 +144,7 @@ class Program
         id = person_data.Count;
         temp=[name,born_date];
         person_data.Add(id, temp);
-
+        
         Console.Clear();
         Console.WriteLine("Dados Salvos");
         string[] data = person_data[key: id];
@@ -155,6 +153,204 @@ class Program
         Thread.Sleep(2000);
         Main_menu();
 
+    }
+
+    static void Remove_register()
+    {
+        string input;
+        int index;
+        bool containsValue;
+        Console.Clear();
+        Console.WriteLine("Menu Principal > Gerenciamento de Cadastros > Remover Cadastro");
+        Console.WriteLine("Digite o NOME do cadastro que queira remover.");
+        Console.WriteLine("0 - Voltar");
+        Console.WriteLine("Digite uma opção e aperte 'Enter'.");
+        input = Console.ReadLine();
+        switch(input)
+        {
+            case "0":
+                Console.WriteLine("0 - Voltar");
+                Manage_registers();
+                break;
+            case "":
+                Console.WriteLine("Opção inválida");
+                Remove_register();
+                break;
+        }
+        containsValue = false;
+        index = 0;
+        foreach (string[] value in person_data.Values)
+        {
+            if (value[0] == input)
+            {
+                containsValue = true;
+                break;
+            }
+            else
+            {
+                index ++;
+            }
+        }
+        if (containsValue)
+        {
+            string[] data = person_data[key: index];
+            Console.WriteLine("Name: " + data[0]);
+            Console.WriteLine("Data de nacimento: " + data[1]);
+            Console.WriteLine("1 - Confirmar exclusão");
+            Console.WriteLine("0 - Cancelar e voltar");
+            Console.WriteLine("Digite uma opção e aperte 'Enter'");
+            input = Console.ReadLine();
+            switch(input)
+            {
+            case "1":
+                Console.WriteLine("1 - Confirmar exclusão");
+                person_data.Remove(index);
+                Console.WriteLine("Exclusão confirmada");
+                Manage_registers();
+                break;
+            case "0":
+                Console.WriteLine("0 - Cancelar e voltar");
+                Manage_registers();
+                break;
+            default:
+                Console.WriteLine("saindo.");
+                Manage_registers();
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Cadastro não encontrado.");
+            Console.WriteLine("aperte 'Enter' para continuar.");
+            input = Console.ReadLine();
+            switch(input)
+            {
+            default:
+                Console.WriteLine("saindo.");
+                Manage_registers();
+                break;
+            }
+        }
+    }
+    
+    static void Edit_register()
+    {
+
+        string input;
+        int index;
+        bool containsValue;
+        Console.Clear();
+        Console.WriteLine("Menu Principal > Gerenciamento de Cadastros > Editar Cadastro");
+        Console.WriteLine("Digite o NOME do cadastro que queira editar.");
+        Console.WriteLine("0 - Voltar");
+        Console.WriteLine("Digite uma opção e aperte 'Enter'.");
+        input = Console.ReadLine();
+        switch(input)
+        {
+            case "0":
+                Console.WriteLine("0 - Voltar");
+                Manage_registers();
+                break;
+            case "":
+                Console.WriteLine("Opção inválida");
+                Remove_register();
+                break;
+        }
+
+        // Check if the dictionary contains a specific value
+        containsValue = false;
+        index = 0;
+        foreach (string[] value in person_data.Values)
+        {
+            if (value[0] == input)
+            {
+                containsValue = true;
+                break;
+            }
+            else
+            {
+                index ++;
+            }
+        }
+        if (containsValue)
+        {
+            string[] data = person_data[key: index];
+            Console.WriteLine("Name: " + data[0]);
+            Console.WriteLine("Data de nacimento: " + data[1]);
+            Console.WriteLine("1 - Editar NOME");
+            Console.WriteLine("2 - Editar DATA");
+            Console.WriteLine("0 - Cancelar e voltar");
+            Console.WriteLine("Digite uma opção e aperte 'Enter'");
+            input = Console.ReadLine();
+            switch(input)
+            {
+            case "1":
+                Console.WriteLine("1 - Editar NOME");
+                Console.WriteLine("Digite o novo valor e aperte 'Enter'");
+                Console.WriteLine("Para cancelar digite '0' e aperte 'Enter'");
+                input = Console.ReadLine();
+                switch(input)
+                {
+                    case "0":
+                        Console.WriteLine("Edição cancelada");
+                        Manage_registers();
+                        break;
+                    case "":
+                        Console.WriteLine("saindo.");
+                        Manage_registers();
+                        break;
+                    
+                }
+                data[0]= input;
+                person_data[index]= [data[0],data[1]]; 
+                Console.WriteLine("Edição confirmada");
+                Manage_registers();
+                break;
+            case "2":
+                Console.WriteLine("2 - Editar DATA");
+                Console.WriteLine("Digite o novo valor e aperte 'Enter'");
+                Console.WriteLine("Para cancelar digite '0' e aperte 'Enter'");
+                input = Console.ReadLine();
+                switch(input)
+                {
+                    case "0":
+                        Console.WriteLine("Edição cancelada");
+                        Manage_registers();
+                        break;
+                    case "":
+                        Console.WriteLine("saindo.");
+                        Manage_registers();
+                        break;
+                    
+                }
+                data[1]= input;
+                person_data[index]= [data[0],data[1]]; 
+                Console.WriteLine("Edição confirmada");
+                Manage_registers();
+                break;
+            case "0":
+                Console.WriteLine("0 - Cancelar e voltar");
+                Manage_registers();
+                break;
+            default:
+                Console.WriteLine("saindo.");
+                Manage_registers();
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Cadastro não encontrado.");
+            Console.WriteLine("aperte 'Enter' para continuar.");
+            input = Console.ReadLine();
+            switch(input)
+            {
+            default:
+                Console.WriteLine("saindo.");
+                Manage_registers();
+                break;
+            }
+        }
     }
 
     static void Show_register()
@@ -179,7 +375,7 @@ class Program
                 Show_register();
                 break;
         }
-                // Check if the dictionary contains a specific value
+        // Check if the dictionary contains a specific value
         containsValue = false;
         index = 0;
         foreach (string[] value in person_data.Values)
